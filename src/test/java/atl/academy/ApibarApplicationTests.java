6837942245fc9d5ab0e972e2b1477a2a5605a4a8
giniblanco <blancogini@gmail.com>
2023-07-25 00:38:21 +0100

    inicio del proyecto apibar

---
+++ src/test/java/atl/academy/ApibarApplicationTests.java
@@ -0,0 +1,13 @@
+package atl.academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApibarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
