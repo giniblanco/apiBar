1d0fac488cfbdf5907137b3ebe975f8d7e6c1e82
martin <mm389358@gmail.com>
2023-07-26 11:48:10 -0300

    The "deleteUser" method has been implemented in the "UserController" class.

--- src/main/java/atl/academy/controllers/UserController.java
+++ src/main/java/atl/academy/controllers/UserController.java
@@ -3,6 +3,7 @@ package atl.academy.controllers;
 import atl.academy.models.User;
 import atl.academy.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +17,10 @@ public class UserController {
     @PostMapping("/api/bar")
     public void registerUser(@RequestBody User user){
         userService.registerUser(user);
+    }
+
+    @DeleteMapping("/api/bar/{id}")
+    public void deleteUser(Long id){
+        userService.deleteUser(id);
     }
 }
