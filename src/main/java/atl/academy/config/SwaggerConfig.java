76d6cd98a88ad4036255a687441806587af622f5
José <jose@recemed.cl>
2023-08-10 19:37:02 -0400

    implementar swagger

---
+++ src/main/java/atl/academy/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package atl.academy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API BAR")
+                        .version("v1")
+                        .description("API BAR - SELECCION DE BEBIDAS Y BUEN GUSTO :)"));
+    }
+}
