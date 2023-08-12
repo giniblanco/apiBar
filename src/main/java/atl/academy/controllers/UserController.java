4be3bfd4527a8fdacb5c1aa0d5cec64a28dac42b
Dominic Roman <romandominic30@gmail.com>
2023-08-11 23:10:46 -0500

    Update and GetAll Users

--- src/main/java/atl/academy/controllers/UserController.java
+++ src/main/java/atl/academy/controllers/UserController.java
@@ -2,9 +2,9 @@ package atl.academy.controllers;
 
 import atl.academy.models.User;
 import atl.academy.services.UserService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -42,5 +42,30 @@ public class UserController {
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable("id") Long id){
         userService.deleteUser(id);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAll(){
+        List<User> barList = userService.getAll();
+        if(barList.isEmpty()){
+            return ResponseEntity.notFound().build();
+        } else{
+            return ResponseEntity.status(HttpStatus.OK).body(barList);
+        }
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user){
+        if(!id.equals(user.getId())){
+            return ResponseEntity.badRequest().build();
+        }
+
+        User updated = userService.update(id, user);
+
+        if(updated != null){
+            return ResponseEntity.ok(updated);
+        } else{
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/atl/academy/services/UserService.java
+++ src/main/java/atl/academy/services/UserService.java
@@ -1,15 +1,17 @@
 package atl.academy.services;
 
 import atl.academy.models.User;
-import atl.academy.repositories.UserRepository;
+import atl.academy.repositories.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
 
     @Autowired
-    UserRepository userRepository;
+    IUserRepository userRepository;
 
 
     public void registerUser(User user) {
@@ -18,5 +20,19 @@ public class UserService {
 
     public void deleteUser(Long id){
         userRepository.deleteById(id);
+    }
+
+    public List<User> getAll(){
+        return userRepository.findAll();
+    }
+
+    public User update(Long id, User user){
+        User existingUser = userRepository.findById(id).orElse(null);
+
+        if(existingUser != null){
+            return userRepository.save(user);
+        }
+
+        return null;
     }
 }
