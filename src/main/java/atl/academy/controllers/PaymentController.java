49adaa887723563b898adc1e2dd72f280e70d02e
José <jose@recemed.cl>
2023-08-26 19:32:18 -0400

    fix ruta paymentController

--- src/main/java/atl/academy/controllers/PaymentController.java
+++ src/main/java/atl/academy/controllers/PaymentController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/api/bar")
+@RequestMapping("/api/payment")
 public class PaymentController {
 
     @Autowired
